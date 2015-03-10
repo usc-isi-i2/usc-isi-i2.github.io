bbeed76e8b0f4a13a1aa0fa152bf7d4660ce4e0a
Haohan <ysyanghaohan@gmail.com>
2015-03-09 20:42:42 -0700

    update

--- dig/src/js/index.js
+++ dig/src/js/index.js
@@ -1,22 +1,42 @@
 // Open external links in new tab
 $('a').each(function() {
    if(this.host !== window.location.host) {
-   		$(this).attr("target", "_blank");
+   		$(this).attr('target', '_blank');
    }
 });
 
+var navBarHeight = $('.dig-nav-bar').outerHeight();
+
 function adjustSlidesHeight() {
-	var width = $(".slides").width();
+	var width = $('.slides').width();
 	var height = width * 0.8;
 
-	if (height > $(window).height()) {
-		height = $(window).height();
+	if (height > $(window).height() - navBarHeight) {
+		height = $(window).height() - navBarHeight;
 	}
 
-	$(".slides").css("height", height + "px");
+	$('.slides').css('height', height + 'px');
 }
 
 adjustSlidesHeight();
 
-$(window).on("resize", adjustSlidesHeight);
+$(window).on('resize', adjustSlidesHeight);
 
+// smooth scroll
+$('a[href*=#]:not([href=#])').click(function() {
+	if (location.pathname.replace(/^\//,'') === this.pathname.replace(/^\//,'') && location.hostname === this.hostname) {
+		var target = $(this.hash);
+		if (target.length) {
+			$('html, body').animate({
+				scrollTop: target.offset().top - navBarHeight
+			}, 500);
+			return false;
+		}
+	}
+});
+$('a[href=#]').click(function() {
+	$('html, body').animate({
+		scrollTop: 0
+	}, 500);
+	return false;
+});
