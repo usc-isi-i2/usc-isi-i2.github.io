c085758b7e130ea663548d610281febe4178f7da
Haohan <ysyanghaohan@gmail.com>
2015-04-09 16:19:26 -0700

    update

---
+++ lodstories/Gruntfile.js
@@ -0,0 +1,76 @@
+module.exports = function(grunt) {
+
+	grunt.initConfig({
+
+		clean: {
+			css: ['css/*.css'],
+		},
+
+		htmllint: {
+			all: ['*.html']
+		},
+
+		sass: {
+			options: {
+				style: 'expanded',
+				sourcemap: 'none'
+			},
+			multiple: {
+				expand: true,
+				flatten: true,
+				src: 'sass/*.scss',
+				dest: 'css',
+				ext: '.css'
+			}
+		},
+
+		autoprefixer: {
+			multiple: {
+				expand: true,
+				flatten: true,
+				src: 'css/*.css',
+				dest: 'css',
+			}
+		},
+
+		jshint: {
+			all: 'js/*.js',
+			options: {
+				curly: true,
+				eqnull: true,
+				eqeqeq: true,
+				browser: true
+			}
+		},
+
+		watch: {
+			options: {
+				livereload: true,
+			},
+			html: {
+				files: ['*.html'],
+				tasks: ['htmllint']
+			},
+			sass: {
+				files: ['sass/*.scss'],
+				tasks: ['clean:css', 'sass', 'autoprefixer']
+			},
+			js: {
+				files: ['js/*.js'],
+				tasks: ['jshint']
+			}
+		}
+		
+	});
+
+grunt.loadNpmTasks('grunt-contrib-clean');
+grunt.loadNpmTasks('grunt-html');
+grunt.loadNpmTasks('grunt-contrib-sass');
+grunt.loadNpmTasks('grunt-autoprefixer');
+grunt.loadNpmTasks('grunt-contrib-jshint');
+grunt.loadNpmTasks('grunt-contrib-watch');
+
+grunt.registerTask('default', ['watch']);
+grunt.registerTask('all', ['clean', 'htmllint', 'sass', 'autoprefixer', 'jshint']);
+
+};
