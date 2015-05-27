1e832d4265ab6abc58e89fd0d9e95f1e5f68d21d
Haohan <ysyanghaohan@gmail.com>
2015-05-27 13:30:35 -0700

    update

---
+++ home/js/index.js
@@ -0,0 +1,39 @@
+// Open external links in new tab
+$('a').each(function() {
+   if(this.host !== window.location.host) {
+   		$(this).attr('target', '_blank');
+   }
+});
+
+$('body').scrollspy({ target: '#nav-links' });
+
+(function normalizeCarouselHeight() {
+	var height = $('#carousel .item.active').height();
+	var $images = $('#carousel .item img');
+
+    $images.each(function() {
+    	$(this).css("height", height);
+    });
+})();
+
+// add title attribute if name or title overflows
+$('#people').find('.name, .title').each(function() {
+	if (this.scrollWidth > this.clientWidth) {
+		$(this).attr('title', $(this).text());
+	}
+});
+
+$('a.scroll').click(function() {
+	// smooth scroll on a large screen
+	if ($(window).width() >= 768) {
+		if (location.pathname.replace(/^\//,'') === this.pathname.replace(/^\//,'') && location.hostname === this.hostname) {
+			var target = $(this.hash);
+			if (target.length) {
+				$('html, body').animate({
+					scrollTop: target.offset().top
+				}, 500);
+				return false;
+			}
+		}
+	}
+});
